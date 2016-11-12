563b34bbf486dda8bed0c5ab94477498d5f4b957
Denis O <denis@universal-development.com>
2016-11-13 01:16:06 +0200

    SQLite  manager service

--- polydata-manager-sqlite/src/main/java/com/unidev/polydata/PolyManagerSQLite.java
+++ polydata-manager-sqlite/src/main/java/com/unidev/polydata/PolyManagerSQLite.java
@@ -1,7 +1,68 @@
 package com.unidev.polydata;
 
+import com.unidev.polydata.domain.Poly;
+import com.unidev.polydata.manager.PolyManager;
+import com.unidev.polydata.service.PolyService;
+import com.unidev.polydata.storage.PolyStorage;
+
+import java.util.HashMap;
+import java.util.Map;
+
 /**
  * SQLite polydata manager
  */
-public class PolyManagerSQLite {
+public class PolyManagerSQLite implements PolyManager {
+
+    private SQLiteStorage polyStorage;
+    private Map<String, PolyService> services = new HashMap<>();
+
+    public PolyManagerSQLite(SQLiteStorage sqLiteStorage) {
+        this.polyStorage = sqLiteStorage;
+    }
+
+    @Override
+    public PolyStorage storage() {
+        return polyStorage;
+    }
+
+    @Override
+    public Map<String, PolyService> services() {
+        return services;
+    }
+
+    public void registerService(PolyService polyService) {
+        services.put(polyService.name(), polyService);
+    }
+
+    @Override
+    public <P extends Poly> P persist(P poly) {
+        Poly existingPoly = polyStorage.fetchById(poly._id());
+
+        polyStorage.persist(poly);
+
+        SQLitePolyServiceVO sqLitePolyServiceVO = new SQLitePolyServiceVO();
+        sqLitePolyServiceVO.setPoly(poly);
+        sqLitePolyServiceVO.setSqLiteStorage(polyStorage);
+
+        services.values().forEach( service -> {
+            if (existingPoly == null) {
+                service.polyAdded(sqLitePolyServiceVO);
+            } else {
+                service.polyRemoved(sqLitePolyServiceVO);
+            }
+        });
+
+        return poly;
+    }
+
+    @Override
+    public <P extends Poly> void remove(P poly) {
+        polyStorage.remove(poly._id());
+
+        SQLitePolyServiceVO sqLitePolyServiceVO = new SQLitePolyServiceVO();
+        sqLitePolyServiceVO.setPoly(poly);
+        sqLitePolyServiceVO.setSqLiteStorage(polyStorage);
+
+        services.values().forEach( service -> service.polyRemoved(sqLitePolyServiceVO));
+    }
 }
