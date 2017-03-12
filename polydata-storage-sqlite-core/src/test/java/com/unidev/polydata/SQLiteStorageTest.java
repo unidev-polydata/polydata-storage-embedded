a0df4d3d06adff98377081d75830a07b5ebe498b
Denis O <denis@universal-development.com>
2017-03-12 22:43:28 +0200

    WIP on sqlite tests

--- polydata-storage-sqlite-core/src/test/java/com/unidev/polydata/SQLiteStorageTest.java
+++ polydata-storage-sqlite-core/src/test/java/com/unidev/polydata/SQLiteStorageTest.java
@@ -20,6 +20,8 @@ import java.util.*;
  */
 public class SQLiteStorageTest {
 
+
+
     @Before
     public void setup() {
         new File("/tmp/testdb.db").delete();
@@ -30,119 +32,119 @@ public class SQLiteStorageTest {
         SQLiteStorage sqLiteStorage = new SQLiteStorage("/tmp/testdb.db");
         sqLiteStorage.migrateStorage();
     }
-
-    @Test
-    public void testMetadataFetching() throws SQLiteStorageException {
-        SQLiteStorage sqLiteStorage = new SQLiteStorage("/tmp/testdb.db");
-        sqLiteStorage.migrateStorage();
-
-        Optional<Poly> tomato = sqLiteStorage.fetchMetadata("tomato");
-        assertThat(tomato.isPresent(), is(false));
-
-        BasicPoly basicPoly = new BasicPoly();
-        basicPoly._id("tomato");
-        basicPoly.put("test_key", "test_value");
-
-        sqLiteStorage.persistMetadata("tomato", basicPoly);
-
-
-        Optional<Poly> updatedTomato = sqLiteStorage.fetchMetadata("tomato");
-        assertThat(updatedTomato.isPresent(), is(true));
-        Poly tomatoPoly = updatedTomato.get();
-
-        assertThat(tomatoPoly._id(), is("tomato"));
-        assertThat(tomatoPoly.get("test_key"), is("test_value"));
-
-
-        Poly metadataPoly = sqLiteStorage.metadata();
-        assertThat(metadataPoly, is(nullValue()));
-
-        BasicPoly metadataToUpdate = new BasicPoly()._id("meta1");
-        sqLiteStorage.metadata(metadataToUpdate);
-
-        Poly changedMetadata = sqLiteStorage.metadata();
-        assertThat(changedMetadata, is(notNullValue()));
-        assertThat(changedMetadata._id(), is("meta1"));
-
-    }
-
-
-    @Test
-    public void testSaveUpdate() throws SQLiteStorageException {
-        SQLiteStorage sqLiteStorage = new SQLiteStorage("/tmp/testdb.db");
-        sqLiteStorage.migrateStorage();
-
-
-        BasicPoly basicPoly = BasicPoly.newPoly("potato");
-        basicPoly.put("value", "tomato");
-
-        Poly poly = sqLiteStorage.fetchById("potato");
-        assertThat(poly, is(nullValue()));
-
-        sqLiteStorage.persist(basicPoly);
-
-        Poly dbPoly = sqLiteStorage.fetchById("potato");
-        assertThat(dbPoly, is(notNullValue()));
-        assertThat(dbPoly.get("value"), is("tomato"));
-
-
-        BasicPoly updatePoly = BasicPoly.newPoly("potato");
-        updatePoly.put("value", "tomato2");
-        sqLiteStorage.persist(updatePoly);
-
-        Poly updatedPoly = sqLiteStorage.fetchById("potato");
-        assertThat(updatedPoly, is(notNullValue()));
-        assertThat(updatedPoly.get("value"), is("tomato2"));
-    }
-
-    @Test
-    public void testPolyRemoval() throws SQLiteStorageException {
-
-        SQLiteStorage sqLiteStorage = new SQLiteStorage("/tmp/testdb.db");
-        sqLiteStorage.migrateStorage();
-
-
-        boolean missingRemoval = sqLiteStorage.remove("potato");
-        assertThat(missingRemoval, is(false));
-
-        BasicPoly basicPoly = BasicPoly.newPoly("potato");
-        basicPoly.put("value", "tomato");
-
-        sqLiteStorage.persist(basicPoly);
-
-        boolean removalResult = sqLiteStorage.remove("potato");
-        assertThat(removalResult, is(true));
-
-        boolean missingRemoval2 = sqLiteStorage.remove("potato");
-        assertThat(missingRemoval2, is(false));
-    }
-
-    @Test
-    public void testPolyListing() {
-        SQLiteStorage sqLiteStorage = new SQLiteStorage("/tmp/testdb.db");
-        sqLiteStorage.migrateStorage();
-
-        assertThat(sqLiteStorage.size(), is(0L));
-
-        Collection<? extends Poly> list = sqLiteStorage.list();
-        assertThat(list.isEmpty(), is(true));
-
-        BasicPoly basicPoly = BasicPoly.newPoly("potato");
-        basicPoly.put("value", "tomato");
-
-        sqLiteStorage.persist(basicPoly);
-
-        assertThat(sqLiteStorage.size(), is(1L));
-        Collection<? extends Poly> records = sqLiteStorage.list();
-        assertThat(records.isEmpty(), is(false));
-        assertThat(records.size(), is(1));
-
-        Poly poly = records.iterator().next();
-        assertThat(poly, is(not(nullValue())));
-        assertThat(poly._id(), is("potato"));
-        assertThat(poly.get("value"), is("tomato"));
-
-
-    }
+//
+//    @Test
+//    public void testMetadataFetching() throws SQLiteStorageException {
+//        SQLiteStorage sqLiteStorage = new SQLiteStorage("/tmp/testdb.db");
+//        sqLiteStorage.migrateStorage();
+//
+//        Optional<Poly> tomato = sqLiteStorage.fetchMetadata("tomato");
+//        assertThat(tomato.isPresent(), is(false));
+//
+//        BasicPoly basicPoly = new BasicPoly();
+//        basicPoly._id("tomato");
+//        basicPoly.put("test_key", "test_value");
+//
+//        sqLiteStorage.persistMetadata("tomato", basicPoly);
+//
+//
+//        Optional<Poly> updatedTomato = sqLiteStorage.fetchMetadata("tomato");
+//        assertThat(updatedTomato.isPresent(), is(true));
+//        Poly tomatoPoly = updatedTomato.get();
+//
+//        assertThat(tomatoPoly._id(), is("tomato"));
+//        assertThat(tomatoPoly.get("test_key"), is("test_value"));
+//
+//
+//        Poly metadataPoly = sqLiteStorage.metadata();
+//        assertThat(metadataPoly, is(nullValue()));
+//
+//        BasicPoly metadataToUpdate = new BasicPoly()._id("meta1");
+//        sqLiteStorage.metadata(metadataToUpdate);
+//
+//        Poly changedMetadata = sqLiteStorage.metadata();
+//        assertThat(changedMetadata, is(notNullValue()));
+//        assertThat(changedMetadata._id(), is("meta1"));
+//
+//    }
+//
+//
+//    @Test
+//    public void testSaveUpdate() throws SQLiteStorageException {
+//        SQLiteStorage sqLiteStorage = new SQLiteStorage("/tmp/testdb.db");
+//        sqLiteStorage.migrateStorage();
+//
+//
+//        BasicPoly basicPoly = BasicPoly.newPoly("potato");
+//        basicPoly.put("value", "tomato");
+//
+//        Poly poly = sqLiteStorage.fetchById("potato");
+//        assertThat(poly, is(nullValue()));
+//
+//        sqLiteStorage.persist(basicPoly);
+//
+//        Poly dbPoly = sqLiteStorage.fetchById("potato");
+//        assertThat(dbPoly, is(notNullValue()));
+//        assertThat(dbPoly.get("value"), is("tomato"));
+//
+//
+//        BasicPoly updatePoly = BasicPoly.newPoly("potato");
+//        updatePoly.put("value", "tomato2");
+//        sqLiteStorage.persist(updatePoly);
+//
+//        Poly updatedPoly = sqLiteStorage.fetchById("potato");
+//        assertThat(updatedPoly, is(notNullValue()));
+//        assertThat(updatedPoly.get("value"), is("tomato2"));
+//    }
+//
+//    @Test
+//    public void testPolyRemoval() throws SQLiteStorageException {
+//
+//        SQLiteStorage sqLiteStorage = new SQLiteStorage("/tmp/testdb.db");
+//        sqLiteStorage.migrateStorage();
+//
+//
+//        boolean missingRemoval = sqLiteStorage.remove("potato");
+//        assertThat(missingRemoval, is(false));
+//
+//        BasicPoly basicPoly = BasicPoly.newPoly("potato");
+//        basicPoly.put("value", "tomato");
+//
+//        sqLiteStorage.persist(basicPoly);
+//
+//        boolean removalResult = sqLiteStorage.remove("potato");
+//        assertThat(removalResult, is(true));
+//
+//        boolean missingRemoval2 = sqLiteStorage.remove("potato");
+//        assertThat(missingRemoval2, is(false));
+//    }
+//
+//    @Test
+//    public void testPolyListing() {
+//        SQLiteStorage sqLiteStorage = new SQLiteStorage("/tmp/testdb.db");
+//        sqLiteStorage.migrateStorage();
+//
+//        assertThat(sqLiteStorage.size(), is(0L));
+//
+//        Collection<? extends Poly> list = sqLiteStorage.list();
+//        assertThat(list.isEmpty(), is(true));
+//
+//        BasicPoly basicPoly = BasicPoly.newPoly("potato");
+//        basicPoly.put("value", "tomato");
+//
+//        sqLiteStorage.persist(basicPoly);
+//
+//        assertThat(sqLiteStorage.size(), is(1L));
+//        Collection<? extends Poly> records = sqLiteStorage.list();
+//        assertThat(records.isEmpty(), is(false));
+//        assertThat(records.size(), is(1));
+//
+//        Poly poly = records.iterator().next();
+//        assertThat(poly, is(not(nullValue())));
+//        assertThat(poly._id(), is("potato"));
+//        assertThat(poly.get("value"), is("tomato"));
+//
+//
+//    }
 
 }
--- polydata-storage-sqlite-core/src/test/java/com/unidev/polydata/SQLiteTest.java
+++ polydata-storage-sqlite-core/src/test/java/com/unidev/polydata/SQLiteTest.java
@@ -13,51 +13,51 @@ import java.util.Map;
  */
 public class SQLiteTest {
 
-    @Test
-    public void testJson() throws ClassNotFoundException, SQLException, JsonProcessingException {
-        Class.forName("org.sqlite.JDBC");
-
-        Connection connection = DriverManager.getConnection("jdbc:sqlite:sample.db");
-        Statement statement = connection.createStatement();
-        statement.setQueryTimeout(30);  // set timeout to 30 sec.
-
-        ObjectMapper objectMapper = new ObjectMapper();
-
-
-
-
-        statement.executeUpdate("drop table if exists poly");
-        statement.executeUpdate("create table poly (id TEXT, json JSON)");
-
-        Map<String, String> object = new HashMap<>();
-        object.put("key1", "qwe");
-        object.put("key2", "123");
-        String rawJson = objectMapper.writeValueAsString(object);
-
-        statement.executeUpdate("insert into poly values('potato', '"+rawJson+"')");
-
-        object = new HashMap<>();
-        object.put("key1", "xyz");
-        object.put("key2", "123");
-        rawJson = objectMapper.writeValueAsString(object);
-
-        statement.executeUpdate("insert into poly values('tomato', '"+rawJson+"' )");
-        ResultSet rs = statement.executeQuery("select * from poly");
-        //rs.getMetaData().getColumn
-        while(rs.next())
-        {
-            // read the result set
-            System.out.println("id = " + rs.getObject("id"));
-            System.out.println("json = " + rs.getObject("json"));
-        }
-
-        System.out.println("***Json query***");
-        rs = statement.executeQuery("select json_extract(poly.json, '$.key1') as data from poly ;");
-        while(rs.next())
-        {
-
-            System.out.println("data = " + rs.getObject("data")  + " " + rs.getObject("data").getClass());
-        }
-
-    }
+//    @Test
+//    public void testJson() throws ClassNotFoundException, SQLException, JsonProcessingException {
+//        Class.forName("org.sqlite.JDBC");
+//
+//        Connection connection = DriverManager.getConnection("jdbc:sqlite:sample.db");
+//        Statement statement = connection.createStatement();
+//        statement.setQueryTimeout(30);  // set timeout to 30 sec.
+//
+//        ObjectMapper objectMapper = new ObjectMapper();
+//
+//
+//
+//
+//        statement.executeUpdate("drop table if exists poly");
+//        statement.executeUpdate("create table poly (id TEXT, json JSON)");
+//
+//        Map<String, String> object = new HashMap<>();
+//        object.put("key1", "qwe");
+//        object.put("key2", "123");
+//        String rawJson = objectMapper.writeValueAsString(object);
+//
+//        statement.executeUpdate("insert into poly values('potato', '"+rawJson+"')");
+//
+//        object = new HashMap<>();
+//        object.put("key1", "xyz");
+//        object.put("key2", "123");
+//        rawJson = objectMapper.writeValueAsString(object);
+//
+//        statement.executeUpdate("insert into poly values('tomato', '"+rawJson+"' )");
+//        ResultSet rs = statement.executeQuery("select * from poly");
+//        //rs.getMetaData().getColumn
+//        while(rs.next())
+//        {
+//            // read the result set
+//            System.out.println("id = " + rs.getObject("id"));
+//            System.out.println("json = " + rs.getObject("json"));
+//        }
+//
+//        System.out.println("***Json query***");
+//        rs = statement.executeQuery("select json_extract(poly.json, '$.key1') as data from poly ;");
+//        while(rs.next())
+//        {
+//
+//            System.out.println("data = " + rs.getObject("data")  + " " + rs.getObject("data").getClass());
+//        }
+//
+//    }
 }
