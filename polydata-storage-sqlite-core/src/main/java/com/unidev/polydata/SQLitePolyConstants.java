dbc780401515e9476dcb1477f6f3a1ec96b97ee2
Denis O <denis@universal-development.com>
2017-03-12 12:12:52 +0200

    Added sqlite poly constants

---
+++ polydata-storage-sqlite-core/src/main/java/com/unidev/polydata/SQLitePolyConstants.java
@@ -0,0 +1,37 @@
+/**
+ * Copyright (c) 2017 Denis O <denis.o@linux.com>
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.unidev.polydata;
+
+import com.fasterxml.jackson.databind.DeserializationFeature;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.SerializationFeature;
+
+/**
+ * Constant keys used in poly records
+ */
+public class SQLitePolyConstants {
+
+    public static final String ID_KEY = "_id";
+    public static final String DATE_KEY = "date";
+    public static final String DATA_KEY = "data";
+    public static final String COUNT_KEY = "count";
+
+    public static ObjectMapper POLY_OBJECT_MAPPER = new ObjectMapper() {{
+        configure(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES, false);
+        configure(SerializationFeature.FAIL_ON_EMPTY_BEANS, false);
+    }};
+
+}
