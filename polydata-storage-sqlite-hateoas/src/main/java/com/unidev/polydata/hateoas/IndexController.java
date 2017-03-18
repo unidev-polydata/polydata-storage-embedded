b5366d6f6bcc67e520ee28df3797957abebc898f
Denis O <denis@universal-development.com>
2017-03-18 16:31:45 +0200

    Added API method for tags fetching

--- polydata-storage-sqlite-hateoas/src/main/java/com/unidev/polydata/hateoas/IndexController.java
+++ polydata-storage-sqlite-hateoas/src/main/java/com/unidev/polydata/hateoas/IndexController.java
@@ -3,6 +3,7 @@ package com.unidev.polydata.hateoas;
 import com.unidev.polydata.SQLitePolyQuery;
 import com.unidev.polydata.SQLiteStorage;
 import com.unidev.polydata.StorageService;
+import com.unidev.polydata.domain.BasicPoly;
 import com.unidev.polydata.domain.bucket.BasicPolyBucket;
 import com.unidev.polydata.exception.NotFoundException;
 import com.unidev.polydata.model.HateoasResponse;
@@ -13,6 +14,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.hateoas.ResourceSupport;
 import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 import static com.unidev.polydata.model.HateoasResponse.hateoasResponse;
 import static org.springframework.hateoas.mvc.ControllerLinkBuilder.linkTo;
@@ -65,5 +68,15 @@ public class IndexController {
 
         ListResponse listResponse = storageService.queryPoly(storage, polyQuery);
         return hateoasResponse().data(listResponse);
+    }
+
+    @GetMapping(value = "/storage/{storage}/tags", produces= MediaType.APPLICATION_JSON_VALUE)
+    public HateoasResponse tags(@PathVariable("storage") String storage) {
+        if (!storageService.existStorageRoot(storage)) {
+            LOG.warn("Not found storage {}", storage);
+            throw new NotFoundException("Storage " + storage + " not found");
+        }
+        List<BasicPoly> tags = storageService.fetchTags(storage);
+        return hateoasResponse().data(tags);
     }
 }
