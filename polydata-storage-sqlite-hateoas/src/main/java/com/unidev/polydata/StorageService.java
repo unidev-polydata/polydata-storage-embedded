7c3c4968f6f569b8da79e75ceec12c9e2bb18208
Denis O <denis@universal-development.com>
2017-03-18 16:27:49 +0200

    Added API method for fetching records

--- polydata-storage-sqlite-hateoas/src/main/java/com/unidev/polydata/StorageService.java
+++ polydata-storage-sqlite-hateoas/src/main/java/com/unidev/polydata/StorageService.java
@@ -1,18 +1,28 @@
 package com.unidev.polydata;
 
 import com.unidev.platform.common.exception.UnidevRuntimeException;
+import com.unidev.polydata.domain.BasicPoly;
 import com.unidev.polydata.domain.bucket.BasicPolyBucket;
+import com.unidev.polydata.model.ListResponse;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 
 import java.io.File;
 import java.io.FileReader;
+import java.sql.Connection;
+import java.sql.SQLException;
+import java.util.List;
 
 /**
  * Service for managing poly records
  */
 @Service
 public class StorageService {
+
+    private static Logger LOG = LoggerFactory.getLogger(SQLiteStorage.class);
+
 
     public static final String DB_FILE = "polydata.db";
     public static final String JSON_FILE = "polydata.json";
@@ -42,6 +52,42 @@ public class StorageService {
             return SQLitePolyConstants.POLY_OBJECT_MAPPER.readValue(fileReader, BasicPolyBucket.class);
         } catch (Exception e) {
             throw new UnidevRuntimeException(e);
+        }
+    }
+
+    public ListResponse queryPoly(String storage, SQLitePolyQuery query) {
+        SQLiteStorage sqLiteStorage = sqLiteStorage(storage);
+        try (Connection connection = sqLiteStorage.openDb()) {
+
+            ListResponse listResponse = new ListResponse();
+            List<BasicPoly> polyList = sqLiteStorage.listPoly(connection, query);
+            long count = sqLiteStorage.fetchPolyCount(connection, query);
+            listResponse.setList(polyList);
+            listResponse.setCount(count);
+            return listResponse;
+        } catch (SQLException e) {
+            LOG.warn("Failed to query polys {} {}", storage, query, e);
+            throw new SQLiteStorageException(e);
+        }
+    }
+
+    public List<BasicPoly> fetchTags(String storage) {
+        SQLiteStorage sqLiteStorage = sqLiteStorage(storage);
+        try (Connection connection = sqLiteStorage.openDb()) {
+            return sqLiteStorage.fetchTags(connection);
+        } catch (SQLException e) {
+            LOG.warn("Failed to fetchTags {} ", storage, e);
+            throw new SQLiteStorageException(e);
+        }
+    }
+
+    public List<BasicPoly> fetchTagsIndex(String storage, String tag) {
+        SQLiteStorage sqLiteStorage = sqLiteStorage(storage);
+        try (Connection connection = sqLiteStorage.openDb()) {
+            return sqLiteStorage.fetchTagIndex(connection, tag);
+        } catch (SQLException e) {
+            LOG.warn("Failed to fetchTagsIndex {} {} ", storage, tag, e);
+            throw new SQLiteStorageException(e);
         }
     }
 
