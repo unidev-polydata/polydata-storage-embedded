eeae89dc15bb04ee1ab5dd70ed458ee10729ef2f
Denis O <denis@universal-development.com>
2017-03-26 00:17:56 +0200

    Added index on update date

--- polydata-storage-sqlite-core/src/main/resources/db/sqlitestorage/V1.0.3__tags_table.sql
+++ polydata-storage-sqlite-core/src/main/resources/db/sqlitestorage/V1.0.3__tags_table.sql
@@ -3,3 +3,5 @@ UPDATE data SET create_date = datetime();
 
 ALTER TABLE data ADD  update_date datetime ;
 UPDATE data SET update_date = datetime();
+
+CREATE INDEX update_date_idx ON data (update_date);
